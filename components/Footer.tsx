69d644c93c4b2994bd2a91b59715da09995d7e5f
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2024-07-27 20:12:07 +0530

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,8 +9,7 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/">
-            <Image src="./orci.png" alt="logo" width={80} height={50} />
-            <h1 className="font-medium text-xl pl-4">ORCI</h1>
+            <h1 className="font-thin text-4xl">ORCI</h1>
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,7 @@ const Navbar = () => {
   return (
     <nav className="max-container padding-container relative z-30 pt-5">
       <Link href="/">
-        <Image src="./orci.png" alt="logo" width={80} height={50}/>
-        <h1 className="font-medium text-xl pl-4">ORCI</h1>
+        <h1 className="font-light text-5xl pt-4">ORCI</h1>
       </Link>
     </nav>
   )
